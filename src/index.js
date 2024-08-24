2cd139daa810ee5ee2681b5c969b67d4b6854620
Houssine <ekhay1@outlook.com>
2024-08-23 23:42:24 -0400

    added pizza, header, footer and menu component and added the style

--- src/index.js
+++ src/index.js
@@ -1,10 +1,112 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
+
+// data
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
 
 function App() {
-  return <h1>Hello ReactDOM</h1>;
+  return (
+    <div className="container">
+      <h1>Hello ReactDOM</h1>;
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+function Header() {
+  return (
+    <header className="header">
+      <h1
+        style={{ color: "red", fontSize: "48px", textTransform: "uppercase" }}
+      >
+        Fast React Pizza Co.
+      </h1>
+    </header>
+  );
+}
+
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>Our menu</h2>
+      <Pizza />
+    </main>
+  );
+}
+
+function Footer() {
+  //   return React.createElement("footer", null, "Wea are currently open");
+  const hour = new Date().getHours();
+  const openHour = 8;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  return (
+    <footer className="footer">
+      {new Date().toLocaleTimeString()}. We are currentley open
+    </footer>
+  );
+}
+
+function Pizza() {
+  return (
+    <div>
+      <img src="pizzas/focaccia.jpg" alt="Pizza spinach" />
+      <h3>Focaccia</h3>
+      <p>Bread with italian olive oil and rosemary </p>
+    </div>
+  );
+}
